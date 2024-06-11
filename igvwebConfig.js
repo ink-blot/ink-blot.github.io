0ac78894df9d8f6d013a4ab56da9fc5cdbe237ee
ink-blot <54012814+ink-blot@users.noreply.github.com>
2024-06-11 21:25:10 +1000

    Add files via upload

---
+++ igvwebConfig.js
@@ -0,0 +1,35 @@
+var igvwebConfig = {
+
+    genomes: "resources/genomes.json",
+    trackRegistryFile: "resources/tracks/trackRegistry.json",
+    sessionRegistryFile: "resources/sessions/sessionRegistry.json",
+
+    // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
+    dropboxAPIKey: "test",
+    
+     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
+    clientId: "test.apps.googleusercontent.com",
+    apiKey: "test",
+
+    // Provide a URL shorterner function or object.   This is optional.  If not supplied
+    // sharable URLs will not be shortened .
+    urlShortener: {
+        provider: "tinyURL"
+    },
+
+    enableCircularView: true,
+
+    restoreLastGenome: true,
+
+    igvConfig:
+        {
+            genome: "hg19",
+            locus: "all",
+            genomeList: "resources/genomes.json",
+            queryParametersSupported: true,
+            showChromosomeWidget: true,
+            showSVGButton: false,
+            tracks: []
+        }
+
+}
