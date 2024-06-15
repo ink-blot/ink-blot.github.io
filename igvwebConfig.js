2a6f73630b514f7f16f34813f117ccd56f1c5771
ink-blot <54012814+ink-blot@users.noreply.github.com>
2024-06-16 09:36:20 +1000

    Update igvwebConfig.js

--- igvwebConfig.js
+++ igvwebConfig.js
@@ -5,17 +5,18 @@ var igvwebConfig = {
     sessionRegistryFile: "resources/sessions/sessionRegistry.json",
 
     // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
-    dropboxAPIKey: "8glijwyao9fq8we",
-    
-     // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
-    clientId: "test.apps.googleusercontent.com",
-    apiKey: "test",
+    //dropboxAPIKey: "...",
+
+    // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
+    //clientId: "...",
+    // apiKey: "...",
 
     // Provide a URL shorterner function or object.   This is optional.  If not supplied
-    // sharable URLs will not be shortened .
-    urlShortener: {
-        provider: "tinyURL"
-    },
+    // sharable URLs will not be shortened.  If using tinyURL supply an api token
+    //urlShortener: {
+    //    provider: "tinyURL",
+    //    api_token: "..."
+    //},
 
     enableCircularView: true,
 
